653cbdadc4c9c97f32bf25e75b851478a83f78ed
Alex Subota <as.subota@gmail.com>
2013-10-20 01:53:21 +0300

    keycode dispatcher added

--- src/snake.js
+++ src/snake.js
@@ -8,63 +8,63 @@ window.requestAnimFrame = (function () {
 }());
 var SnakeGame = function (elID) {
 	'use strict';
-    this.canvas = document.createElement('canvas');
-    this.offCanvas = document.createElement('canvas');
-    this.cWidth = 320;
-    this.cHeight = 320;
-    this.canvas.width = this.offCanvas.width = this.cWidth;
-    this.canvas.height = this.offCanvas.height = this.cHeight;
-    this.context = this.canvas.getContext('2d');
-    this.offContext = this.offCanvas.getContext('2d');
-    this.container = document.querySelector(elID);
-    this.raf = null;
+	this.canvas = document.createElement('canvas');
+	this.offCanvas = document.createElement('canvas');
+	this.cWidth = 320;
+	this.cHeight = 320;
+	this.canvas.width = this.offCanvas.width = this.cWidth;
+	this.canvas.height = this.offCanvas.height = this.cHeight;
+	this.context = this.canvas.getContext('2d');
+	this.offContext = this.offCanvas.getContext('2d');
+	this.container = document.querySelector(elID);
+	this.raf = null;
 
-    this.init();
+	this.init();
 };
 SnakeGame.prototype = {
-    init: function () {
-        var Snake = function (length) {
-            var i;
-            this.segment = {
-                width: 10,
-                height: 10,
-                img: null
-            };
-            this.skinColor = 'green';
-            this.skinColor2 = 'cyan';
-            this.speed = 1;
-            this.dirX = 1;
-            this.dirY = 0;
-            this.lng = length;
-            this.body = [];
-            for (i = 0; i < this.lng; i++) {
-                this.body.push([i, 0]);
-            }
-
-            return this;
-        };
-        Snake.prototype = {
-            prepareImgs: function (ctx) {
-                ctx.save();
-                ctx.fillStyle = this.skinColor2;
-                ctx.fillRect(0, 0, this.segment.width, this.segment.height);
-                ctx.fillStyle = this.skinColor;
-                ctx.fillRect(1, 1, this.segment.width - 2, this.segment.height - 2);
-                ctx.restore();
-                this.segment.img = ctx.getImageData(0,0,this.segment.width,this.segment.height);
-            },
-            draw: function (ctx) {
-                var self = this,
-                    i = 0;
-                for (i; i < self.body.length; i++) {
-                    ctx.putImageData(self.segment.img, self.body[i][0] * self.segment.width, self.body[i][1] * self.segment.height);
+	init: function () {
+		var game = this,
+            Snake = function (length) {
+                var i;
+                this.segment = {
+                    width: 10,
+                    height: 10,
+                    img: null
+                };
+                this.skinColor = 'green';
+                this.skinColor2 = 'cyan';
+                this.speed = 1;
+                this.dirX = 1;
+                this.dirY = 0;
+                this.lng = length;
+                this.body = [];
+                for (i = 0; i < this.lng; i++) {
+                    this.body.push([i, 0]);
                 }
-            },
-            control: function (e) {
-                console.log(this);
+
+                return this;
+            };
+		Snake.prototype = {
+			prepareImgs: function (ctx) {
+				ctx.save();
+				ctx.fillStyle = this.skinColor2;
+				ctx.fillRect(0, 0, this.segment.width, this.segment.height);
+				ctx.fillStyle = this.skinColor;
+				ctx.fillRect(1, 1, this.segment.width - 2, this.segment.height - 2);
+				ctx.restore();
+				this.segment.img = ctx.getImageData(0,0,this.segment.width,this.segment.height);
+			},
+			draw: function (ctx) {
+				var self = this,
+					i = 0;
+				for (i; i < self.body.length; i++) {
+					ctx.putImageData(self.segment.img, self.body[i][0] * self.segment.width, self.body[i][1] * self.segment.height);
+				}
+			},
+            control: function (key) {
                 var dirX, dirY,
                     self = this;
-                switch (e.which) {
+                switch (key) {
                     case 87: //up
                         dirX = 0;
                         dirY = -1;
@@ -89,45 +89,72 @@ SnakeGame.prototype = {
                 }
                 self.dirY = dirY; self.dirX = dirX;
             }
-        };
+		};
 
-        this.snake = new Snake(10);
-        this.snake.prepareImgs(this.offContext);
-        this.container.appendChild(this.canvas);
-    },
-    run: function () {
-        var self = this,
-            lastTime = 0,
-            callback = function (t) {
-                if (t - lastTime > 500) {
+		this.snake = new Snake(10);
+		this.snake.prepareImgs(this.offContext);
+		this.container.appendChild(this.canvas);
+
+        this.keyHandler = {
+            handleEvent: function (e) {
+                this.addKey(e);
+            },
+            dispatchKey: function () {
+                if (this.queue.length) {
+                    game.snake.control(this.queue.pop());
+                }
+            },
+            addKey: function (e) {
+                if (this.queue.length < this.maxQueue) {
+                    this.queue.unshift(e.which);
+                }
+            },
+            queue: [],
+            maxQueue: 3
+        };
+	},
+	run: function () {
+		var self = this,
+			lastTime = 0,
+			callback = function (t) {
+				if (t - lastTime > 500) {
                     self.animLoop();
                     lastTime = t;
-                }
-                self.raf = window.requestAnimationFrame(callback);
-            };
-        this.raf = window.requestAnimationFrame(callback);
-    },
-    stop: function () {
-        window.cancelAnimationFrame(this.raf);
-    },
-    clear: function () {
-        this.context.clearRect(0,0,this.cWidth,this.cHeight);
-    },
-    draw: function () {
-         this.snake.draw(this.context);
-    },
-    update: function () {
-        var snakeLength = this.snake.body.length;
-        for (var i = 0; i < snakeLength - 1; i++) {
-            this.snake.body[i][0] = this.snake.body[i + 1][0];
-            this.snake.body[i][1] = this.snake.body[i + 1][1];
-        }
-        this.snake.body[snakeLength - 1][0] += this.snake.dirX;
-        this.snake.body[snakeLength - 1][1] += this.snake.dirY;
-    },
-    animLoop: function () {
+				}
+				self.raf = window.requestAnimationFrame(callback);
+			};
+
         this.clear();
         this.draw();
+
+        window.addEventListener('keydown', this.keyHandler, false);
+		this.raf = window.requestAnimationFrame(callback);
+	},
+	stop: function () {
+		window.cancelAnimationFrame(this.raf);
+        window.removeEventListener('keydown', this.keyHandler, false);
+    },
+	clear: function () {
+		this.context.clearRect(0,0,this.cWidth,this.cHeight);
+	},
+	draw: function () {
+		this.snake.draw(this.context);
+	},
+	update: function () {
+		var snakeLength = this.snake.body.length,
+            i = 0;
+
+        this.keyHandler.dispatchKey();
+        for (i; i < snakeLength - 1; i++) {
+			this.snake.body[i][0] = this.snake.body[i + 1][0];
+			this.snake.body[i][1] = this.snake.body[i + 1][1];
+		}
+		this.snake.body[snakeLength - 1][0] += this.snake.dirX;
+		this.snake.body[snakeLength - 1][1] += this.snake.dirY;
+	},
+	animLoop: function () {
         this.update();
+        this.clear();
+		this.draw();
     }
 };
